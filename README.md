8ead32ee24e4bbdcefd8dcbf2a3a755ee5a167e5
Ishaan Shringi <ishaanshringi96@gmail.com>
2019-07-27 16:48:36 +0530

    Authentication using JWT

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # numbers_to_words_api
 NodeJs API converts numeric currency into words (INR)
 
-For dev : npm run dev
+## Execution
 
-For build : npm run prod
+Step 1 :-
+
+     For dev : npm run dev
+
+     For build : npm run prod
+
+Step 2 :-
+
+     Make a GET request to following url
+     http://localhost:5000/get_api_key
+     You will get a token in response.
+
+Step 3 :-
+
+     Make a POST request to
+     http://localhost:5000/to_words
+     with token in body.
+
+     Example Body :
+     {
+          "number":236482343,
+          "token":"eyJhbGciOiJIUzI1NiJ9.ZGpnaGhoaHV1d2l3dWV3aWV1d2lldXJpd3U.EG5jX0Q5riihDkn7a0n_jFQ5Ca842qpUEsy8xMoGoq4"
+     }
+
+     You will receive following in response :
+     {
+          "success": true,
+          "words": "Twenty Three Crore Sixty Four Lakh Eighty Two Thousand Three Hundred Forty Three Rupees"
+     }
--- app.ts
+++ app.ts
@@ -2,8 +2,8 @@ import express from "express"
 import bodyParser from "body-parser"
 import compression from "compression"
 import converter from 'convert-rupees-into-words'
-
-const API_KEY = ""
+import jwt from 'jsonwebtoken'
+import key from './key'
 
 const app = express()
 
@@ -11,30 +11,50 @@ app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-     res.json({
-          key: API_KEY
-     })
+
+app.use((req, res, next) => {
+     if(req.path==='/get_api_key') {
+          next()
+     }
+     else {
+          jwt.verify(req.body.token, key.tokenKey, (err, payload) => {
+               if(payload && payload===key.tokenKey) {
+                    next()
+               }
+               else {
+                    res.status(400).send("Access Denied")
+               }
+          })
+     }
 })
 
+
 app.get("/get_api_key", (req, res) => {
+     var token = jwt.sign(key.tokenKey, key.tokenKey)
      res.json({
-          data: API_KEY
+          token
      })
 })
 
 app.post("/to_words", (req, res) => {
-     let number: number = parseInt(req.body.number)
-     if(typeof number === "number") {
-          res.json({
-               success: true,
-               words: converter(number)
-          })
-     }
-     else {
+     try {
+          let number: number = req.body.number
+          if (Number.isInteger(number)) {
+               res.json({
+                    success: true,
+                    words: converter(number)
+               })
+          }
+          else 
           res.json({
                success: false,
-               error: ""
+               error: 'Invalid number in body'
+          })
+     }
+     catch(err) {
+          res.json({
+               success: false,
+               error: err
           })
      }
 })
